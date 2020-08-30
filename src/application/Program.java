5e1e4c56f6d8fc2805e46c547880b6c4813c3f1a
Rafael De Luca <deluca1712@gmail.com>
2020-08-30 12:17:36 -0300

    Posicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class Program {
-	
-	public static void main (String [] args) {
-		
-		System.out.println("Systema de xadre inicial");
-		
+
+	public static void main(String[] args) {
+
+		System.out.println("Systema de xadrez inicial");
+
+		Posicao pos = new Posicao(3, 6);
+		System.out.println(pos);
+
 	}
-	
+
 }
